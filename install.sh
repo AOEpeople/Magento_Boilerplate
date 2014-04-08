65073129ce67d7e734b54f7335972746f3f7e58f
Fabrizio Branca <fabrizio.branca@aoe.com>
2014-04-08 14:15:27 -0700

    Simplified installer by using EnvSettingsTool's new group feature

--- install.sh
+++ install.sh
@@ -11,29 +11,20 @@ cd ${SOURCE_DIR} && tools/composer.phar install || (echo "Composer failed"; exit
 cd ${SOURCE_DIR} && tools/modman deploy-all --force || (echo "Modman failed"; exit 1)
 
 # Importing systemstorage
-MASTER_SYSTEM="devbox"
-ENVIRONMENT="devbox"
+    MASTER_SYSTEM="devbox"
 
-## Database
-# This requires the local.xml already to have all correct parameter. But this happens later in 'Run EnvSettingsTool'. That again requires the db to already be in place
-# cd ${SOURCE_DIR}/htdocs && ../tools/n98-magerun.phar db:import --compression=gzip ../systemstorage/demo/database/combined_dump.sql.gz
-# That's why we do it like this for now (and might introduce a way to set the local.xml stuff only via EnvSettings before doing this in the future)
-DB_HOST=`${SOURCE_DIR}/tools/value.php ${ENVIRONMENT} ${SOURCE_DIR}/Configuration/settings.csv Est_Handler_XmlFile app/etc/local.xml /config/global/resources/default_setup/connection/host`
-DB_DBNAME=`${SOURCE_DIR}/tools/value.php ${ENVIRONMENT} ${SOURCE_DIR}/Configuration/settings.csv Est_Handler_XmlFile app/etc/local.xml /config/global/resources/default_setup/connection/dbname`
-DB_USERNAME=`${SOURCE_DIR}/tools/value.php ${ENVIRONMENT} ${SOURCE_DIR}/Configuration/settings.csv Est_Handler_XmlFile app/etc/local.xml /config/global/resources/default_setup/connection/username`
-DB_PASSWORD=`${SOURCE_DIR}/tools/value.php ${ENVIRONMENT} ${SOURCE_DIR}/Configuration/settings.csv Est_Handler_XmlFile app/etc/local.xml /config/global/resources/default_setup/connection/password`
+    # Run EnvSettingsTool (processing only rows tagged with 'db')
+    cd ${SOURCE_DIR}/htdocs && ../tools/apply.php devbox ../Configuration/settings.csv --groups db || (echo "EnvSettingsTool failed"; exit 1)
 
-mysql -u${DB_USERNAME} -p${DB_PASSWORD} -h${DB_HOST} -BNe "show tables" "${DB_DBNAME}" | tr '\n' ',' | sed -e 's/,$//' | awk '{print "SET FOREIGN_KEY_CHECKS = 0;DROP TABLE IF EXISTS " $1 ";SET FOREIGN_KEY_CHECKS = 1;"}' | mysql -u${DB_USERNAME} -p${DB_PASSWORD} -h${DB_HOST} "${DB_DBNAME}"
+    ## Database
+    cd ${SOURCE_DIR}/htdocs && ../tools/n98-magerun.phar db:import --compression=gzip ../systemstorage/devbox/database/combined_dump.sql.gz
 
-gunzip < "${SOURCE_DIR}/systemstorage/${MASTER_SYSTEM}/database/combined_dump.sql.gz" | sed -e 's/\/\*[^*]*DEFINER=[^*]*\*\///' | mysql -u${DB_USERNAME} -p${DB_PASSWORD} -h${DB_HOST} ${DB_DBNAME} || exit 1
+    ## Files
+    rsync --force --no-o --no-p --no-g --omit-dir-times --ignore-errors --archive --partial \
+        --exclude=/catalog/product/cache/ --exclude=/tmp/ --exclude=.svn/ --exclude=*/.svn/ --exclude=.git/ --exclude=*/.git/ \
+        "${SOURCE_DIR}/systemstorage/${MASTER_SYSTEM}/files/" "${SOURCE_DIR}/htdocs/media/"
 
-
-## Files
-rsync --force --no-o --no-p --no-g --omit-dir-times --ignore-errors --archive --partial \
-    --exclude=/catalog/product/cache/ --exclude=/tmp/ --exclude=.svn/ --exclude=*/.svn/ --exclude=.git/ --exclude=*/.git/ \
-    "${SOURCE_DIR}/systemstorage/${MASTER_SYSTEM}/files/" "${SOURCE_DIR}/htdocs/media/"
-
-# Run EnvSettingsTool
+# Run EnvSettingsTool (processing all)
 cd ${SOURCE_DIR}/htdocs && ../tools/apply.php devbox ../Configuration/settings.csv || (echo "EnvSettingsTool failed"; exit 1)
 
 echo
