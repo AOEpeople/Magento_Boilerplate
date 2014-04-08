d5ce58aa3266dcc0d337ebd1a6470ae0e490df56
Fabrizio Branca <fabrizio.branca@aoe.com>
2014-04-07 22:43:27 -0700

    Added basic installer

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Get absolute path to main directory
+ABSPATH=$(cd "${0%/*}" 2>/dev/null; echo "${PWD}/${0##*/}")
+SOURCE_DIR=`dirname "${ABSPATH}"`
+
+cd ${SOURCE_DIR} 
+
+# Run composer
+./tools/composer.phar install || (echo "Composer failed"; exit 1)
+
+# Run modman
+./tools/modman deploy-all --force || (echo "Modman failed"; exit 1)
+
+# Run EnvSettingsTool
+cd htdocs
+./tools/apply.php devbox ../Configuration/settings.csv || (echo "EnvSettingsTool failed"; exit 1)
+
+echo "Installation successful!"
+
